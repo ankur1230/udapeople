b2702ea6c99852f0e44f9cab45bc2ac1e196e193
Ankur Khandelwal <ankurkhandelwal30@gmail.com>
2023-04-08 11:41:58 +0530

    update

--- backend/revert-migrations.sh
+++ backend/revert-migrations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-SUCCESS = $(curl --insecure https://kvdb.io/26CFVW9rVsg3weDY3qP1q9/migration_${CIRCLE_WORKFLOW_ID:0:7})
-if(( $SUCCESS==1 )); 
+SUCCESS="$(curl --insecure https://kvdb.io/26CFVW9rVsg3weDY3qP1q9/migration_"${CIRCLE_WORKFLOW_ID:0:7}")"
+if(( SUCCESS==1 )); 
 then
     npm install
     npm run migrations:revert
