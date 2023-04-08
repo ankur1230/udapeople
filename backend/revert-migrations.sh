6373765a4ed225824ba1430f3c507c6bf44762ca
Ankur Khandelwal <ankurkhandelwal30@gmail.com>
2023-04-08 11:33:58 +0530

    Test fix

---
+++ backend/revert-migrations.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+SUCCESS = $(curl --insecure https://kvdb.io/26CFVW9rVsg3weDY3qP1q9/migration_${CIRCLE_WORKFLOW_ID:0:7})
+if(( $SUCCESS==1 )); 
+then
+    npm install
+    npm run migrations:revert
+fi
